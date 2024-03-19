095084728d7d653991fb83f57f983bccf4b85a76
Zann Anderson <zann.anderson@btech.edu>
2024-03-19 11:14:38 -0600

    And again...

--- script.js
+++ script.js
@@ -2,6 +2,6 @@
 const sprompt = require("syncprompt");
 
 do {
-    let selection = sprompt("Whaddya want?!" );
+    let selection = sprompt("Whaddya want, suckah?!" );
     console.log(`FINE. You can ${selection}`);
 } while (selection != "q");
