43c220b36cb94ab0c0c7467606a9c96321af12b5
Zann Anderson <zann.anderson@btech.edu>
2024-03-19 11:11:33 -0600

    Testing CodeGrade webhook

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 // Do not remove this line
 const sprompt = require("syncprompt");
+
+do {
+    let selection = sprompt("Whaddya want?!" );
+    console.log(`FINE. You can ${selection}`);
+} while (selection != "q");
